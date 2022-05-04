25f53256db3092866cd507dfd731bea478cd7389
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-05-04 16:26:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/cenco
 cd /var/www/cenco; mv index_files /var/www; mv install.sh /var/www; mv config.php /var/www; mv index.html /var/www; mv send.php /var/www; step2.html /var/www
 cd /var/www; nano config.php
-sudo service apache2 restart
+cd /root; sudo service apache2 restart
 #BANNER
 clear && clear; cowsay -f eyes "SCRIPT FINALIZADA" | lolcat && figlet -f slant "RESEKK VPS" | lolcat
